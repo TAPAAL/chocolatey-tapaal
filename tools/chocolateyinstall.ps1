42f8506687c47a201b267673ebe0ed1ad1c4c072
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2021-03-01 14:37:30 +0100

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://download.tapaal.net/tapaal/tapaal-3.7/tapaal-3.7.1-win64.zip'
+$url64      = 'http://download.tapaal.net/tapaal/tapaal-3.7/tapaal-3.7.1-win64.zip'
 $checksum64  = '5672F7A143B6127D0D6957B2C6BCF0CB7A32221B165265E3681BDF3E5FE212B5'
 
 $packageArgs = @{
@@ -16,5 +16,5 @@ $packageArgs = @{
   checksumType64= 'sha256' 
 
 }
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 # Attemped workarround for TSL 1.2 not working on chocolatey validation servers, due to powershell 4.0 
+
 Install-ChocolateyZipPackage @packageArgs 
