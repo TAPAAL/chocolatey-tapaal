22e330922dcb549d90a849ada6dbdff9148bc1a2
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2022-11-13 10:22:49 +0100

    Updated version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://download.tapaal.net/tapaal/tapaal-3.9/tapaal-3.9.2-win64.zip'
+$url64      = 'https://download.tapaal.net/tapaal/tapaal-3.9/tapaal-3.9.3-win64.zip'
 $checksum64  = '090D66AC20B5B73A0E2AFADB058A6E2F9DA89EE125249E454FFAEDF358F82B85'
 
 $packageArgs = @{
