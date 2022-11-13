c08fdcd7900ce1cd9131ec3abc267a5cc6f6ab78
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2022-11-13 10:29:52 +0100

    updated checksum

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url64      = 'https://download.tapaal.net/tapaal/tapaal-3.9/tapaal-3.9.3-win64.zip'
-$checksum64  = '090D66AC20B5B73A0E2AFADB058A6E2F9DA89EE125249E454FFAEDF358F82B85'
+$checksum64  = 'E6DB9669D8A9934E0B87AD50EF6DA952913E4BB4DAD94A6407432F34F41A8E88'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
